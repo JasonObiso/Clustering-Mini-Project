cb31f738a2414161b274b30b6aa11edb534a8647
JasonCarloObiso <144089877+JasonObiso@users.noreply.github.com>
2024-04-29 16:55:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
 
 def main():
     st.title('Classification/Clustering App')
